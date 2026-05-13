d40b4820cb3860f7d5d95f8d288e7ce2e0f1986f
Iago Pinheiro <iagoo.pinheiro73@gmail.com>
2026-05-12 21:29:19 -0300

    feat: configurar React Router com layout e rotas das páginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
-import { colors } from "./theme/colors";
-import styles from "./styles/app.module.css";
-import logo from "./assets/images/logo-full.png";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout/MainLayout";
+import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Sobre from "./pages/Sobre/Sobre";
 
 export default function App() {
   return (
-    <main className={styles.container}>
-      <img src={logo} alt="EcoSense Logo" className={styles.logo} />
-      <h1 className={styles.title}>EcoSense</h1>
-      <p className={styles.subtitle}>
-        Monitoramento inteligente para um futuro sustentável
-      </p>
-      <div className={styles.card}>
-        <p>
-          Sistema IoT que coleta dados de corrente e voltagem diretamente dos
-    equipamentos, permitindo acompanhar o consumo de energia, identificar
-    desperdícios e tomar decisões mais eficientes.
-        </p>
-      </div>
-    </main>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Route>
+    </Routes>
   );
 }
