1c5fcd26f5ba1de736638093e6a8c1886103111a
Pedro Xavier <pedro_xavier9@outlook.com>
2026-05-29 20:55:26 -0300

    feat(api): expor endpoint de estatisticas de telemetria

--- backend/src/routes/telemetriaRoutes.js
+++ backend/src/routes/telemetriaRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   registrarTelemetria,
   obterTelemetria,
+  obterEstatisticas,
 } from "../controllers/telemetriaController.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
@@ -12,5 +13,8 @@ router.post("/ingestao", registrarTelemetria);
 
 // Rota de CONSUMO: GET para /api/telemetria/{dispositivoId}
 router.get("/:dispositivoId", verifyToken, obterTelemetria);
+
+// Rota de ESTATÍSTICAS: GET para /api/telemetria/estatisticas/{dispositivoId}
+router.get("/estatisticas/:dispositivoId", verifyToken, obterEstatisticas);
 
 export default router;
